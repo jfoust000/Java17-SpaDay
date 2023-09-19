c9fc3f5c7a507b2ddc1b8f125f56dfb49d59c9a8
Joel Foust <joel.foust@yahoo.com>
2023-09-19 16:56:50 -0400

    need to finish form validation

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,39 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+
+        model.addAttribute("title", "Add User");
+        model.addAttribute(new User());
+        return "user/add";
+
+    }
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+
+        if (verify.equals(user.getPassword())) {
+
+            String uName = user.getUserName();
+            model.addAttribute("user", uName);
+            return "user/index";
+
+        } else {
+
+            return "user/add";
+
+       }
+
+    }
+
+}
